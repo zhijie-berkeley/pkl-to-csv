3ceced7c9c5452ac5a12b075297a76781a54ba48
zhijie <zhijie@berkeley.edu>
2018-06-07 21:14:59 -0700

    shell to convert all unzipping and transcription data to csv (to share with Spakowitz)

---
+++ pkl_to_csv.sh
@@ -0,0 +1,57 @@
+# to give permission: chmod +x pkl_to_csv.sh
+# to run this shell: ./pkl_to_csv.sh
+
+echo start saving pkl to csv ...
+
+echo start saving unzipping bare DNA
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/Bare\ DNA/
+
+echo start saving unzipping H2A.Z nucleosome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/H2A.Z\ Nucleosome/
+
+echo start saving unzipping M3_M7 Nucleosome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/M3_M7\ Nucleosome/
+
+echo start saving unzipping Tetrasome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/Tetrasome/
+
+echo start saving unzipping uH2A Nucleosome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/uH2A\ Nucleosome/
+
+echo start saving unzipping uH2B Nucleosome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/uH2B\ Nucleosome/
+
+echo start saving unzipping wt-H2A Nucleosome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/wt-H2A\ Nucleosome/
+
+echo start saving transcription bare DNA
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Transcription\ data\ analysis/analysis/601-For-300mM-10pN-realignedfulltraces/
+
+echo start saving transcription WT-Nucleosome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Transcription\ data\ analysis/analysis/For-50mM-10pN-realignedfulltraces/
+
+echo start saving transcription H2A.Z-Nucleosome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Transcription\ data\ analysis/analysis/yPolII-hH2A.Z-10pN-realignedfulltraces/
+
+echo start saving transcription uH2B-Nucleosome
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Transcription\ data\ analysis/analysis/yPolII-huH2B-10pN-realignedfulltraces/
+
+echo start saving unzipping Hopping H2AZ
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/Hopping-traces/H2AZ/
+
+echo start saving unzipping Hopping M3_M7
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/Hopping-traces/M3_M7/
+
+echo start saving unzipping Hopping WT
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/Hopping-traces/WT/
+
+echo start saving unzipping Hopping bare_DNA
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/Hopping-traces/bare_DNA/
+
+echo start saving unzipping Hopping uH2B
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/Hopping-traces/uH2B/
+
+echo start saving unzipping Hopping uH2A
+python /Users/ZHIJIE/Google\ Drive/github/pkl-to-csv/pkl_to_csv.py /Users/ZHIJIE/Documents/Data\ Analysis/Unzipping\ data\ analysis/Hopping-traces/uH2A/
+
+echo done
